5054fdda7eb271646a9632c8b2c6cc2a6d82db1e
NellePot <mutambolero@up.edu.ph>
2025-05-29 20:30:24 +0800

    styling for student dashboard and request

--- student_dashboard.php
+++ student_dashboard.php
@@ -39,7 +39,7 @@ $conn -> close();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Student Dashboard</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="dashboardStyle.css">
 </head>
 <body>
     <header>
